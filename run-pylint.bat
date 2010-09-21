16433c758b5c6b86939a50a86a1324429c1598b0
Israel Fruchter <israel.fruchter@gmail.com>
2010-09-22 00:12:49 +0200

    change to batch file

--- run-pylint.bat
+++ run-pylint.bat
@@ -1,2 +1,2 @@
-pylint -f parseable -i y bitgames.py --output-format=parseable --ignore-comments=y --min-similarity-lines=4 > pylint.txt
-exit 0
+pylint  --rcfile=pylint_rc bitgames.py > pylint.txt
+EXIT /b 0
