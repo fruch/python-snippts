6659e581dd2fd4c391f8e5fefcb7fb2d8faccfdf
Israel Fruchter <israel.fruchter@gmail.com>
2010-09-23 23:24:03 +0200

    fix run_pylint for output files

--- run_pylint.py
+++ run_pylint.py
@@ -1,5 +1,5 @@
 import sys
 import pylint.lint
-args = ['bitgames.py','-f', 'parseable','--max-line-length=120']
+args = ['bitgames.py','-f', 'parseable','--max-line-length=120', '--files-output=y']
 
 pylint.lint.Run(args, exit=False)
